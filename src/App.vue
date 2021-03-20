7fadd46e378afbd5e6ff63c46fecf5f2766d7268
Hailong Zhao <hailongzh@hotmail.com>
2021-03-20 17:01:44 -0400

    Creating a Forum Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
+  <the-navbar/>
   <div class="container">
     <router-view/>
   </div>
 </template>
 
 <script>
+import TheNavbar from './components/TheNavbar.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: { TheNavbar }
 }
 </script>
 
