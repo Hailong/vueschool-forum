f760e72a4064557aa438ea1310636fe34c6697d7
Hailong Zhao <hailongzh@hotmail.com>
2021-04-28 07:32:32 -0400

    Firebase Setup and Installation

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: '<FILL YOUR OWN API KEY HERE>',
+  authDomain: 'vue-school-forum-72391.firebaseapp.com',
+  projectId: 'vue-school-forum-72391',
+  storageBucket: 'vue-school-forum-72391.appspot.com',
+  messagingSenderId: '334823958834',
+  appId: '1:334823958834:web:b14815c3695fbf242f4f2b'
+}
