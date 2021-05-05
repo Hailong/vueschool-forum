1aade963d7db7259b48b16aedea258408e31cbd1
Hailong Zhao <hailongzh@hotmail.com>
2021-05-04 22:58:58 -0400

    Fetching Data in beforeCreate on the Homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -13,6 +13,11 @@ export default {
     categories () {
       return this.$store.state.categories
     }
+  },
+  async beforeCreate () {
+    const categories = await this.$store.dispatch('fetchAllCategories')
+    const forumIds = categories.map(category => category.forums).flat()
+    this.$store.dispatch('fetchForums', { ids: forumIds })
   }
 }
 </script>
